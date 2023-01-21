60a2fe9b4999987259154a936053c08472b99650
Vinicius Eloy <paulo.v.almeida@globalhitss.com.br>
2023-01-21 16:59:01 -0300

    Fix Axios network error add local IP

--- mobile/src/lib/axios.ts
+++ mobile/src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333'
+  baseURL: 'http://192.168.1.206:3333' //utilizando o IP local setado no server (server.ts)
 })
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,17 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { appRoutes } from "./routes"
+import { appRoutes } from './routes'
 
 const app = Fastify()
 
-app.register(cors)
+app.register(cors, {
+  origin: true,
+})
 app.register(appRoutes)
 
 app.listen({
   port: 3333,
-}).then(() => {
-  console.log('HTTP Server running!')
+  host: '192.168.1.206', //local IP > usar o mesmo IP na canexão do mobile (axios.ts)
+}).then((url) => {
+  console.log(`HTTP Server running on ${url}`)
 })
